690ae30a28ac150eb5b91004f3337f7e7a466297
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-07-24 09:28:17 +0300

    finish course

---
+++ src/main.js
@@ -0,0 +1,119 @@
+// colleen van lent ph.d
+// todo ((((((((((((((html))))))))))))))
+// 1990-1993 first graphic-1994-1995 netscape 
+
+// web roles 
+// internet engineer task force ietf 
+// world wide web consortium w3c 
+// web accessibility intiative wai
+
+// first browser support img=>mosaic
+//! html 93-95-97-99-2012 html5 [1=>5]
+// html use for html,css,dom features,reduce plugin,more markupScripting
+// todo ((((((((((((((html client server))))))))))))))
+// server share wan \ client personal lan =>req <= res 
+// ftp file transfer protocol 
+
+// domain by icaan international corporation for assigned names and numbers 2012
+
+//internet protocol
+
+//lan local area network
+
+//ipv6 16 vars
+
+// high level domain .com 
+
+// /html.html =>doc 
+
+// ? html => ip req => http =>hidden Headers,cookies,form data ... , server return files not web page what browser decide it 200,404,500 .. 
+
+// todo ((((((((((((((((browser options))))))))))))))))
+// internet explorer -> edge(most access) -> chrome(secure) most use,firefox 
+
+//editors sublime,notepad++
+
+//code pin
+// todo ((((((((((((((((document object model))))))))))))))))
+// dom=>Headers,body from root doctype 
+// validator.w3.com 
+// browser will fix bad code but not always
+// <ol start="30"> <ol reversed>  <li value="9"> li>ul>li*3
+// list values 
+// none disc circle square decimal 
+// tr>td(inline) tr>th,td
+// tags closing,self-close
+//attitude =>block | inline
+// image rarely one first time broken | short src |long src
+// attitude class,id,style.accesskey(shortcut),tabindex(focus important),special entities=>&lt; &copy;
+// header,nav,footer,figure=>figcaption,aside
+// new tags struct article,aside,main,menuitem,summary,section
+//form datalist,keygen,output
+// input color,data,email,list
+//graphic canvas,svg
+//media audio,video,track,source,embed
+// todo ((((((((((((((((template page))))))))))))))))
+//doctype,metadata, 
+// todo ((((((((((((((((img))))))))))))))))
+// img=>src alt ,width,height 50%,alt text,accurate,succinct,have not pic,graph harding value the same all time
+// todo ((((((((((((((((font awesome))))))))))))))))
+// a href="http://www.twitter.com" aria-label="twitter" ><i class="fas fa-angry fa-lg"></i> 
+// todo ((((((((((((((((hyperlink))))))))))))))))
+// a href ,link href a>img _self _blank _top _parent (absolute,relative)
+// todo ((((((((((((((((multimedia))))))))))))))))
+// audio video ,plugins,reflect on accessibility issue
+// video src =>width,height,autoplay,loop,controls <source>
+// audio src =>autoplay,loop,controls buffer,muted,volume
+// setting clips .ext#t=5,25
+//plugin some required flash
+//access issue=>provide link plugin,include text desc and close caption,media enhance content controls
+// todo ((((((((((((((((tables ))))))))))))))))
+//sketch layout before code,design number of rows,cols,describe rows ,cols table>tr,td,th rowspan="2",colspan="2",border="1" use caption not use h1->6
+// todo ((((((((((((((((useful tags ))))))))))))))))
+// genetic p,div semantic=>header ,nav, footer,figure
+// containers =>article,aside,section,main,hr,summary details,blockquote
+// inline span,cite(alt),time,code,sub,sup,abbr(alt)
+//tags button,meter,progress,iframe,bdo=>dir,map <area>
+//meter min="0",max="100",value="50"
+//progress max="100" value="70"
+//iframe src
+// todo ((((((((((((((((accessibility))))))))))))))))
+// help guide policy and purchasing decisions ,eval web interface for access,assists those with disabilities to access online infra
+// keep pace with change tech 
+// 5 issue of people eye,touch,color,hear =>font size,hearable,style
+// make a music for blind 
+// visual ,hear ,motor (can't use arms) steady hand
+// cognitive can't focus on amount of information tbi,ptsd,adhd 16m 
+// more no hear and blind =>touch ,poor,unemployeed
+// opportunities for disabled =>eduction,news ,commerance,social
+
+// legal doj,ada online resource local entities ,law individuals,state nyu,fsu,ramada
+
+//accessibility
+//audio every where ,online infrastructure for adhd,seo ,mobile ,usablitiy
+// accessibility based on pour=>
+// perceivable,operable,understandable,robust 
+// todo ((((((((((((((((validation))))))))))))))))
+//? url address,filename upload,direct input(w3c upload code)
+//? accessibility wave.webaim.org
+// todo ((((((((((((((((wave))))))))))))))))
+// browser extension=> 
+// todo ((((((((((((((((funkify =>run away fair extension))))))))))))))))
+// accessibility extension for validation 
+// todo ((((((((((((((((hosting github))))))))))))))))
+// need to registered ip address to connect with domain name 
+// hosting services  => free,mid-range,full-service
+//free little /no control domain name,limited tools ,ad,redirects,familiar look and feel across them
+//paid support available ,have better tools
+//! sign up bytehost =>remember pass, check spam
+// todo ((((((((((((((((cPanel))))))))))))))))
+//cPanel account need url =>domain name,hosting service (file manager for url is a argent use)
+// ? upload public html to people who show your webpage
+//a2hosting.com =>root public_html
+//error view causes =>url type error,upload error,file uploaded can not have access
+// changes public,less opportunity to test with cPanel
+//cPanel paid have more tools,alterative is sftp(secured ftp) to transfer files
+
+// flashback ,allow to one-at-a-time file upload oppose
+//ftp client win=>winScp , mac =>fugu/cyberduck
+// copy gmail ftp/sftp => connect winScp =>open connect sftp =>server=>
